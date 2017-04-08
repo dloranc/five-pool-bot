31a8d2252edf2262c7f625d5374c305b6f6330c6
Dawid Loranc <dawidloranc@gmail.com>
2017-04-08 19:32:40 +0200

    Initial commit

---
+++ src/BuildingUtilities.java
@@ -0,0 +1,58 @@
+import bwapi.Game;
+import bwapi.TilePosition;
+import bwapi.Unit;
+import bwapi.UnitType;
+
+public class BuildingUtilities {
+    // Returns a suitable TilePosition to build a given building type near
+    // specified TilePosition aroundTile, or null if not found. (builder parameter is our worker)
+    static public TilePosition getBuildTile(Game game, Unit builder, UnitType buildingType, TilePosition aroundTile) {
+        TilePosition ret = null;
+        int maxDist = 3;
+        int stopDist = 40;
+
+        // Refinery, Assimilator, Extractor
+        if (buildingType.isRefinery()) {
+            for (Unit n : game.neutral().getUnits()) {
+                if ((n.getType() == UnitType.Resource_Vespene_Geyser) &&
+                        (Math.abs(n.getTilePosition().getX() - aroundTile.getX()) < stopDist) &&
+                        (Math.abs(n.getTilePosition().getY() - aroundTile.getY()) < stopDist)
+                        ) return n.getTilePosition();
+            }
+        }
+
+        while ((maxDist < stopDist) && (ret == null)) {
+            for (int i = aroundTile.getX() - maxDist; i <= aroundTile.getX() + maxDist; i++) {
+                for (int j = aroundTile.getY() - maxDist; j <= aroundTile.getY() + maxDist; j++) {
+                    if (game.canBuildHere(new TilePosition(i, j), buildingType, builder, false)) {
+                        // units that are blocking the tile
+                        boolean unitsInWay = false;
+                        for (Unit u : game.getAllUnits()) {
+                            if (u.getID() == builder.getID()) continue;
+                            if ((Math.abs(u.getTilePosition().getX() - i) < 4) && (Math.abs(u.getTilePosition().getY() - j) < 4))
+                                unitsInWay = true;
+                        }
+                        if (!unitsInWay) {
+                            return new TilePosition(i, j);
+                        }
+                        // creep for Zerg
+                        if (buildingType.requiresCreep()) {
+                            boolean creepMissing = false;
+                            for (int k = i; k <= i + buildingType.tileWidth(); k++) {
+                                for (int l = j; l <= j + buildingType.tileHeight(); l++) {
+                                    if (!game.hasCreep(k, l)) creepMissing = true;
+                                    break;
+                                }
+                            }
+                            if (creepMissing) continue;
+                        }
+                    }
+                }
+            }
+            maxDist += 2;
+        }
+
+        if (ret == null) game.printf("Unable to find suitable build position for " + buildingType.toString());
+        return ret;
+    }
+}
---
+++ src/FivePoolBot.java
@@ -0,0 +1,275 @@
+import bwapi.*;
+import bwta.BWTA;
+import bwta.BaseLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class FivePoolBot extends DefaultBWListener {
+    private Mirror mirror = new Mirror();
+
+    private Game game;
+
+    private Player self;
+
+    private boolean isSpawningPool;
+    private boolean isScouting;
+    private Unit scoutDrone;
+    private Unit buildDrone;
+    private Unit hatchery;
+    private BaseLocation playerStartLocation;
+    private List<BaseLocation> possibleEnemyBaseLocations;
+    private BaseLocation baseToScout;
+    private BaseLocation enemyBase;
+    private int basesCount;
+
+    public void run() {
+        mirror.getModule().setEventListener(this);
+        mirror.startGame();
+    }
+
+    @Override
+    public void onUnitCreate(Unit unit) {
+        System.out.println("New unit created " + unit.getType());
+    }
+
+    @Override
+    public void onStart() {
+        game = mirror.getGame();
+        self = game.self();
+
+        isScouting = false;
+        isSpawningPool = false;
+        scoutDrone = null;
+        buildDrone = null;
+        hatchery = null;
+        playerStartLocation = null;
+        possibleEnemyBaseLocations = null;
+        baseToScout = null;
+        enemyBase = null;
+
+        //Use BWTA to analyze map
+        //This may take a few minutes if the map is processed first time!
+        System.out.println("Analyzing map...");
+        BWTA.readMap();
+        BWTA.analyze();
+        System.out.println("Map data ready");
+
+        int i = 0;
+        for (BaseLocation baseLocation : BWTA.getBaseLocations()) {
+            System.out.println("Base location #" + (++i) + ". Printing location's region polygon:");
+            for (Position position : baseLocation.getRegion().getPolygon().getPoints()) {
+                System.out.print(position + ", ");
+            }
+            System.out.println();
+        }
+
+        game.setLocalSpeed(0);
+
+        playerStartLocation = BWTA.getStartLocation(self);
+        possibleEnemyBaseLocations = BWTA.getStartLocations();
+
+        basesCount = possibleEnemyBaseLocations.size();
+        removePlayerBaseFromBaseList();
+
+        for (Unit myUnit : self.getUnits()) {
+            if (scoutDrone == null) {
+                if (myUnit.getType() == UnitType.Zerg_Drone) {
+                    scoutDrone = myUnit;
+                }
+            }
+
+            if (buildDrone == null) {
+                if (myUnit.getType() == UnitType.Zerg_Drone && !myUnit.equals(scoutDrone)) {
+                    buildDrone = myUnit;
+                }
+            }
+
+            if (hatchery == null) {
+                if (myUnit.getType() == UnitType.Zerg_Hatchery) {
+                    hatchery = myUnit;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onFrame() {
+        float supplyTotal = self.supplyTotal() / 2;
+        float supplyUsed = self.supplyUsed() / 2;
+
+        //game.setTextSize(10);
+        game.drawTextScreen(10, 10, "Playing as " + self.getName() + " - " + self.getRace());
+
+        int enemyCount = game.enemy().getUnits().size();
+
+        game.drawTextScreen(10, 25,
+                game.mapName() + ", " + basesCount + " bases, enemy units: " + enemyCount
+                        + ", bases to scout: " + possibleEnemyBaseLocations.size()
+        );
+
+        if (scoutDrone != null) {
+            game.drawTextScreen(10, 45, "Scout Drone: " + scoutDrone.getPosition().toString());
+        }
+
+        if (buildDrone != null) {
+            game.drawTextScreen(10, 65, "Build Drone: " + buildDrone.getPosition().toString());
+        }
+
+        int dronesCount = 0;
+
+        for (Unit myUnit : self.getUnits()) {
+            if (myUnit.getType() == UnitType.Zerg_Drone) {
+                dronesCount++;
+            }
+        }
+
+        for (Unit myUnit : self.getUnits()) {
+            if (myUnit.getType() == UnitType.Zerg_Hatchery) {
+                if (supplyTotal - supplyUsed <= 1) {
+                    if (self.minerals() >= 100) {
+                        myUnit.train(UnitType.Zerg_Overlord);
+                    }
+                } else {
+                    if (supplyUsed < 5) {
+                        if (self.minerals() >= 50) {
+                            myUnit.train(UnitType.Zerg_Drone);
+                        }
+                    } else {
+                        if (self.minerals() >= 50) {
+                            myUnit.train(UnitType.Zerg_Zergling);
+                        }
+                    }
+                }
+            }
+
+            if (dronesCount >= 5 && !isScouting) {
+                baseToScout = selectBase();
+                game.printf(baseToScout.getPosition().toString());
+                scoutDrone.attack(baseToScout.getPosition());
+                isScouting = true;
+            }
+
+            gatherMinerals(myUnit);
+
+            if (enemyBase != null) {
+                if (myUnit.getType() == UnitType.Zerg_Zergling && myUnit.isIdle()) {
+                    myUnit.attack(enemyBase.getPosition());
+//                    List<Unit> enemyUnits = game.enemy().getUnits();
+//
+//                    int buildings = 0;
+//
+//                    for (Unit enemy : enemyUnits) {
+//                        if (enemy.getType().isBuilding()) {
+//                            myUnit.attack(enemy.getPosition());
+//                            buildings++;
+//                            break;
+//                        }
+//                    }
+//
+//                    game.drawTextScreen(10, 85, "Enemy buildings: " + buildings);
+                }
+            }
+        }
+
+        if (isScouting) {
+            List<Unit> enemyUnits = game.enemy().getUnits();
+
+            int buildingsCount = 0;
+
+            for (Unit enemyUnit : enemyUnits) {
+                if (enemyUnit.getType().isBuilding()) {
+                    buildingsCount++;
+                }
+            }
+
+            if (scoutDrone.isUnderAttack()) {
+                enemyBase = baseToScout;
+                backToBaseToGatherMinerals();
+            }
+
+            if (scoutDrone.isIdle() && buildingsCount == 0) {
+                possibleEnemyBaseLocations.remove(baseToScout);
+
+                baseToScout = selectBase();
+                scoutDrone.attack(baseToScout.getPosition());
+            }
+        }
+
+        if (dronesCount >= 5 && !isSpawningPool && self.minerals() >= 200) {
+            TilePosition buildPosition = BuildingUtilities.getBuildTile(
+                    game,
+                    buildDrone,
+                    UnitType.Zerg_Spawning_Pool,
+                    playerStartLocation.getTilePosition()
+            );
+
+            if (buildPosition != null) {
+                buildDrone.build(UnitType.Zerg_Spawning_Pool, buildPosition);
+                isSpawningPool = true;
+            }
+        }
+    }
+
+    private void backToBaseToGatherMinerals() {
+        Unit closestMineral = null;
+
+        //find the closest mineral near main hatchery
+        for (Unit neutralUnit : game.neutral().getUnits()) {
+            if (neutralUnit.getType().isMineralField()) {
+                if (closestMineral == null || hatchery.getDistance(neutralUnit) < hatchery.getDistance(closestMineral)) {
+                    closestMineral = neutralUnit;
+                }
+            }
+        }
+
+        scoutDrone.gather(closestMineral);
+    }
+
+    private BaseLocation selectBase() {
+        int baseIndex = ThreadLocalRandom.current().nextInt(possibleEnemyBaseLocations.size());
+
+        BaseLocation baseLocation = possibleEnemyBaseLocations.get(baseIndex);
+
+        return baseLocation;
+    }
+
+    private void removePlayerBaseFromBaseList() {
+        List<BaseLocation> toRemove = new ArrayList<>();
+
+        for (BaseLocation location : possibleEnemyBaseLocations) {
+            if (location.getPosition().equals(playerStartLocation.getPosition())) {
+                toRemove.add(location);
+            }
+        }
+
+        possibleEnemyBaseLocations.removeAll(toRemove);
+    }
+
+    private void gatherMinerals(Unit myUnit) {
+        if ((myUnit.getType().isWorker() && myUnit.isIdle())) {
+            if (!(myUnit.equals(scoutDrone) && isScouting)) {
+                Unit closestMineral = null;
+
+                //find the closest mineral
+                for (Unit neutralUnit : game.neutral().getUnits()) {
+                    if (neutralUnit.getType().isMineralField()) {
+                        if (closestMineral == null || myUnit.getDistance(neutralUnit) < myUnit.getDistance(closestMineral)) {
+                            closestMineral = neutralUnit;
+                        }
+                    }
+                }
+
+                //if a mineral patch was found, send the worker to gather it
+                if (closestMineral != null) {
+                    myUnit.gather(closestMineral, false);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new FivePoolBot().run();
+    }
+}
